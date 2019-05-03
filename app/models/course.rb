32bc23033744a68056bdb6e39ae3a589e4cd941e
Hugo Mejia <superboss74@protonmail.com>
2019-05-03 15:31:57 +0000

    Adding Course Validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,8 @@
 # this is the Course class
 class Course < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
