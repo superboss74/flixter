ce61b08ea35d8e036c4346bc24f02ed69b3b1399
Hugo Mejia <superboss74@protonmail.com>
2019-05-10 15:37:21 +0000

    Adding Course Enrollment Requirement to view Lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_current_lesson, only: :show
+
   def show
   end
 
   private
+
+  def require_enrollment_for_current_lesson
+    if !current_user.enrollments.pluck(:course_id).include?(current_lesson.section.course.id)
+      redirect_to course_path(current_lesson.section.course), alert: "Please enroll to this course to watch it's lessons"
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
